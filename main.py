7884dcfa377e4d73179569cb616f9e9beacc004c
Peppe289 <gsperanza204@protonmail.com>
2024-09-05 16:44:01 +0200

    improve python structure
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ import sqlite3
 TOKEN = "7082907773:AAGx-50TmQr_v7KAGsR0H7FSE0FcFA9clR8"
 chat_id = "519932241"
 
-# open database connection
-conn = sqlite3.connect("database.db", timeout=1000)
-cursor = conn.cursor()
-
-cursor.execute("CREATE TABLE IF NOT EXISTS news_history(id INTEGER PRIMARY KEY AUTOINCREMENT, date varchar(50), title TEXT, type TEXT);")
-
-
 def send_post_request(url, data):
     try:
         # Need to seet application/x-www-form-urlencoded header for put and use data string.
@@ -140,41 +133,28 @@ def handler(url, data):
 
     getNewMessage(page_ajax)
 
-url = "https://comune.locri.rc.it/wp-admin/admin-ajax.php"
-# this is data string about request of new news. The page is defined by "post_count=" arg.
-data = "action=load_more&query=%7B%22page%22%3A0%2C%22pagename%22%3A%22novita%22%2C%22error%22%3A%22%22%2C%22m%22%3A%22%22%2C%22p%22%3A0%2C%22post_parent%22%3A%22%22%2C%22subpost%22%3A%22%22%2C%22subpost_id%22%3A%22%22%2C%22attachment%22%3A%22%22%2C%22attachment_id%22%3A0%2C%22name%22%3A%22novita%22%2C%22page_id%22%3A0%2C%22second%22%3A%22%22%2C%22minute%22%3A%22%22%2C%22hour%22%3A%22%22%2C%22day%22%3A0%2C%22monthnum%22%3A0%2C%22year%22%3A0%2C%22w%22%3A0%2C%22category_name%22%3A%22%22%2C%22tag%22%3A%22%22%2C%22cat%22%3A%22%22%2C%22tag_id%22%3A%22%22%2C%22author%22%3A%22%22%2C%22author_name%22%3A%22%22%2C%22feed%22%3A%22%22%2C%22tb%22%3A%22%22%2C%22paged%22%3A0%2C%22meta_key%22%3A%22%22%2C%22meta_value%22%3A%22%22%2C%22preview%22%3A%22%22%2C%22s%22%3A%22%22%2C%22sentence%22%3A%22%22%2C%22title%22%3A%22%22%2C%22fields%22%3A%22%22%2C%22menu_order%22%3A%22%22%2C%22embed%22%3A%22%22%2C%22category__in%22%3A%5B%5D%2C%22category__not_in%22%3A%5B%5D%2C%22category__and%22%3A%5B%5D%2C%22post__in%22%3A%5B%5D%2C%22post__not_in%22%3A%5B%5D%2C%22post_name__in%22%3A%5B%5D%2C%22tag__in%22%3A%5B%5D%2C%22tag__not_in%22%3A%5B%5D%2C%22tag__and%22%3A%5B%5D%2C%22tag_slug__in%22%3A%5B%5D%2C%22tag_slug__and%22%3A%5B%5D%2C%22post_parent__in%22%3A%5B%5D%2C%22post_parent__not_in%22%3A%5B%5D%2C%22author__in%22%3A%5B%5D%2C%22author__not_in%22%3A%5B%5D%2C%22search_columns%22%3A%5B%5D%2C%22ignore_sticky_posts%22%3Afalse%2C%22suppress_filters%22%3Afalse%2C%22cache_results%22%3Atrue%2C%22update_post_term_cache%22%3Atrue%2C%22update_menu_item_cache%22%3Afalse%2C%22lazy_load_term_meta%22%3Atrue%2C%22update_post_meta_cache%22%3Atrue%2C%22post_type%22%3A%22%22%2C%22posts_per_page%22%3A10%2C%22nopaging%22%3Afalse%2C%22comments_per_page%22%3A%2250%22%2C%22no_found_rows%22%3Afalse%2C%22order%22%3A%22DESC%22%7D&page=1&post_count=POST_CLUSTER&load_posts=3&search=&post_types=%22notizia%22&load_card_type=notizia&query_params=%5B%5D&additional_filter=null"
+def main():
+    # open database connection
+    global conn
+    conn = sqlite3.connect("database.db", timeout=1000)
+    global cursor
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS news_history(id INTEGER PRIMARY KEY AUTOINCREMENT, date varchar(50), title TEXT, type TEXT);")
 
-# just check last 3*3 post in 3^3 time. just for make sure XD
-index = 0
-while index < 3:
-    handler(url, data.replace("POST_CLUSTER", str(index * 3)))
-    index += 1
-    print(index)
+    url = "https://comune.locri.rc.it/wp-admin/admin-ajax.php"
+    # this is data string about request of new news. The page is defined by "post_count=" arg.
+    data = "action=load_more&query=%7B%22page%22%3A0%2C%22pagename%22%3A%22novita%22%2C%22error%22%3A%22%22%2C%22m%22%3A%22%22%2C%22p%22%3A0%2C%22post_parent%22%3A%22%22%2C%22subpost%22%3A%22%22%2C%22subpost_id%22%3A%22%22%2C%22attachment%22%3A%22%22%2C%22attachment_id%22%3A0%2C%22name%22%3A%22novita%22%2C%22page_id%22%3A0%2C%22second%22%3A%22%22%2C%22minute%22%3A%22%22%2C%22hour%22%3A%22%22%2C%22day%22%3A0%2C%22monthnum%22%3A0%2C%22year%22%3A0%2C%22w%22%3A0%2C%22category_name%22%3A%22%22%2C%22tag%22%3A%22%22%2C%22cat%22%3A%22%22%2C%22tag_id%22%3A%22%22%2C%22author%22%3A%22%22%2C%22author_name%22%3A%22%22%2C%22feed%22%3A%22%22%2C%22tb%22%3A%22%22%2C%22paged%22%3A0%2C%22meta_key%22%3A%22%22%2C%22meta_value%22%3A%22%22%2C%22preview%22%3A%22%22%2C%22s%22%3A%22%22%2C%22sentence%22%3A%22%22%2C%22title%22%3A%22%22%2C%22fields%22%3A%22%22%2C%22menu_order%22%3A%22%22%2C%22embed%22%3A%22%22%2C%22category__in%22%3A%5B%5D%2C%22category__not_in%22%3A%5B%5D%2C%22category__and%22%3A%5B%5D%2C%22post__in%22%3A%5B%5D%2C%22post__not_in%22%3A%5B%5D%2C%22post_name__in%22%3A%5B%5D%2C%22tag__in%22%3A%5B%5D%2C%22tag__not_in%22%3A%5B%5D%2C%22tag__and%22%3A%5B%5D%2C%22tag_slug__in%22%3A%5B%5D%2C%22tag_slug__and%22%3A%5B%5D%2C%22post_parent__in%22%3A%5B%5D%2C%22post_parent__not_in%22%3A%5B%5D%2C%22author__in%22%3A%5B%5D%2C%22author__not_in%22%3A%5B%5D%2C%22search_columns%22%3A%5B%5D%2C%22ignore_sticky_posts%22%3Afalse%2C%22suppress_filters%22%3Afalse%2C%22cache_results%22%3Atrue%2C%22update_post_term_cache%22%3Atrue%2C%22update_menu_item_cache%22%3Afalse%2C%22lazy_load_term_meta%22%3Atrue%2C%22update_post_meta_cache%22%3Atrue%2C%22post_type%22%3A%22%22%2C%22posts_per_page%22%3A10%2C%22nopaging%22%3Afalse%2C%22comments_per_page%22%3A%2250%22%2C%22no_found_rows%22%3Afalse%2C%22order%22%3A%22DESC%22%7D&page=1&post_count=POST_CLUSTER&load_posts=3&search=&post_types=%22notizia%22&load_card_type=notizia&query_params=%5B%5D&additional_filter=null"
 
-conn.commit()
-cursor.close()
-conn.close()
+    # just check last 3*3 post in 3^3 time. just for make sure XD
+    index = 0
+    while index < 3:
+        handler(url, data.replace("POST_CLUSTER", str(index * 3)))
+        index += 1
+        print(index)
 
-exit()
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-# get from first
-start = page_ajax.find('<div class="col-md-6 col-xl-4">')
-end = page_ajax.find('<div class="col-md-6 col-xl-4">', start + 1)
-
-obj = convert_html_to_obj(page_ajax[start:end])
-
-print(obj["title"])
-
-
-
-
-exit()
-
-# get from second
-start = page_ajax.find('<div class="col-md-6 col-xl-4">', start + 1)
-end = page_ajax.find('<div class="col-md-6 col-xl-4">', start + 1)
-obj = convert_html_to_obj(page_ajax[start:end])
-
-# get last
-start = page_ajax.find('<div class="col-md-6 col-xl-4">', start + 1)
-obj = convert_html_to_obj(page_ajax[start:])
+if __name__ == "__main__":
+    main()
